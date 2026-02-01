68559aab2a61888552b5d009197c2776c5a44950
nkcyborg <kit.heckman@gmail.com>
2026-02-01 11:25:43 -0500

    First timer conversation loops and pass/fail logic

--- Assets/SubmissionPassFailFinder.cs
+++ Assets/SubmissionPassFailFinder.cs
@@ -1,3 +1,4 @@
+using PixelCrushers.DialogueSystem;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,13 +8,53 @@ public class SubmissionPassFailFinder : MonoBehaviour
 {
     [SerializeField]
     public GameManager gameManager;
+    //I hate the way I set this up but whatever it's a game jam. it would look up children game objects by tag if I gave a fuqqq
+    [SerializeField]
+    public DialogueSystemTrigger AlienPass;
+    [SerializeField]
+    public DialogueSystemTrigger AlienFail;
+    [SerializeField]
+    public DialogueSystemTrigger EnbyPass;
+    [SerializeField]
+    public DialogueSystemTrigger EnbyFail;
+    [SerializeField]
+    public DialogueSystemTrigger RobotPass;
+    [SerializeField]
+    public DialogueSystemTrigger RobotFail;
     public void PassOrFail()
     {
-        if(gameManager.GetCurrentClientName() == "Alien")
-        { Debug.Log(gameManager.GetCurrentClientName()); }
-        else
+        if (gameManager.GetCurrentClientName() == "Alien")
         {
-            Debug.Log(gameManager.GetCurrentClientName());
+            if (gameManager.GetClientSuccess() == true)
+            {
+                AlienPass.OnUse();
+            }
+            else
+            {
+                AlienFail.OnUse();
+            }
+        }
+        else if (gameManager.GetCurrentClientName() == "Enby")
+        {
+            if (gameManager.GetClientSuccess() == true)
+            {
+                EnbyPass.OnUse();
+            }
+            else
+            {
+                EnbyFail.OnUse();
+            }
+        }
+        else if (gameManager.GetCurrentClientName() == "Robot")
+        {
+            if (gameManager.GetClientSuccess() == true)
+            {
+                RobotPass.OnUse();
+            }
+            else
+            {
+                RobotFail.OnUse();
+            }
         }
     }
 
